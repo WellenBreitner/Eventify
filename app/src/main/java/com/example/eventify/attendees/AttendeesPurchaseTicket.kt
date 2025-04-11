a3c51bc985c8d60b296f2fd0a080127b4b0ab61e
Wellen Breitner <wellenhan45@gmail.com>
2025-04-11 15:05:08 +0800

    attendess purchase ticket page
    adding number of ticket when attendees select seat

--- app/src/main/java/com/example/eventify/attendees/AttendeesPurchaseTicket.kt
+++ app/src/main/java/com/example/eventify/attendees/AttendeesPurchaseTicket.kt
@@ -1,6 +1,7 @@
 package com.example.eventify.attendees
 
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -13,10 +14,15 @@ import com.example.eventify.R
 import com.example.eventify.attendeesAdapter.SeatAdapter
 
 class AttendeesPurchaseTicket : AppCompatActivity() {
+
     private lateinit var recyclerView: RecyclerView
+    private lateinit var numberOfTicket: TextView
+
     private val seats = mutableListOf<SeatModelData>()
     private val rows = listOf("A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L","AA","BB","CC","DD","EE")
     private val cols = 21+13+13
+    private val bookSeat = ArrayList<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -28,31 +34,14 @@ class AttendeesPurchaseTicket : AppCompatActivity() {
         }
         
         initializeUI()
+        initializeListener()
+    }
+
+    private fun initializeListener() {
     }
 
     private fun initializeUI() {
-        recyclerView = findViewById(R.id.eventSeatSelectionForPurchasesTicket)
-        recyclerView.layoutManager = GridLayoutManager(this,cols)
-        seatSetup()
-        val adapter = SeatAdapter(seats)
-        recyclerView.adapter = adapter
-        recyclerView.setHasFixedSize(true)
-
-        adapter.setSeatOnClick(object:SeatAdapter.seatOnClick{
-            override fun onClick(seatModelData: SeatModelData) {
-                if (!seatModelData.isSelected){
-                    Toast.makeText(this@AttendeesPurchaseTicket, "${seatModelData.label} selected", Toast.LENGTH_SHORT).show()
-                    seatModelData.isSelected = true
-
-                }else{
-                    Toast.makeText(this@AttendeesPurchaseTicket, "${seatModelData.label} unselected", Toast.LENGTH_SHORT).show()
-                    seatModelData.isSelected = false
-                }
-
-            }
-
-        })
-
+        initializeRecycleViewAndSeatSelectionFeature()
 
     }
 
@@ -62,5 +51,33 @@ class AttendeesPurchaseTicket : AppCompatActivity() {
                 seats.add(SeatModelData(row + number,row,number,false))
             }
         }
+    }
+
+    private fun initializeRecycleViewAndSeatSelectionFeature(){
+        recyclerView = findViewById(R.id.eventSeatSelectionForPurchasesTicket)
+        recyclerView.layoutManager = GridLayoutManager(this,cols)
+        seatSetup()
+        val adapter = SeatAdapter(seats)
+        recyclerView.adapter = adapter
+        recyclerView.setHasFixedSize(true)
+
+        numberOfTicket = findViewById(R.id.numberOfPurchaseTicket)
+
+
+        adapter.setSeatOnClick(object:SeatAdapter.seatOnClick{
+            override fun onClick(seatModelData: SeatModelData) {
+                if (!seatModelData.isSelected){
+                    Toast.makeText(this@AttendeesPurchaseTicket, "${seatModelData.label} selected", Toast.LENGTH_SHORT).show()
+                    seatModelData.isSelected = true
+                    bookSeat.add(seatModelData.label)
+
+                }else{
+                    Toast.makeText(this@AttendeesPurchaseTicket, "${seatModelData.label} unselected", Toast.LENGTH_SHORT).show()
+                    seatModelData.isSelected = false
+                    bookSeat.remove(seatModelData.label)
+                }
+                numberOfTicket.text = bookSeat.size.toString()
+            }
+        })
     }
 }
