392c830ccb590c1878d1d94e59dafabae07a83ab
Wellen Breitner <wellenhan45@gmail.com>
2025-04-12 19:49:12 +0800

    attendess purchase ticket page
    1.fixing some bug
    2.adding some detail when user choose ticket type

--- app/src/main/java/com/example/eventify/attendees/AttendeesTicketType.kt
+++ app/src/main/java/com/example/eventify/attendees/AttendeesTicketType.kt
@@ -11,6 +11,7 @@ import android.widget.RadioGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBar.LayoutParams
+import androidx.constraintlayout.widget.Group
 import androidx.core.view.marginTop
 import com.example.eventify.R
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
@@ -27,16 +28,22 @@ class AttendeesTicketType : BottomSheetDialogFragment() {
         val view = inflater.inflate(R.layout.fragment_attendees_ticket_type, container, false)
 
         // testing purpose
-        val array = arrayOf("option1","option2","option3","option4","option5","option6","option7","option8","option9",
-            "option1","option2","option3","option4","option5","option6","option7","option8","option9",
-            "option1","option2","option3","option4","option5","option6","option7","option8","option9")
+        val array = arrayOf("option1","option2","option3","option4","option5","option6","option7","option8","option9","option10",
+            "option11","option12","option13","option14","option15","option16","option17","option18","option19","option20",
+            "option21","option22","option23","option24","option25","option26","option27","option28","option29","option30")
 
         ticketTypeRadioGroup = view.findViewById(R.id.attendeesTicketTypeRadioButton)
-        radioButton = RadioButton(requireActivity())
+
         for (i in array.indices){
+            radioButton = RadioButton(requireActivity())
             radioButton.text = array[i]
+            radioButton.textSize =18f
             radioButton.id = View.generateViewId()
             radioButton.tag = array[i]
+
+            if (array[0] == radioButton.tag.toString()){
+                radioButton.isChecked = true
+            }
 
             val params = RadioGroup.LayoutParams(
                 RadioGroup.LayoutParams.MATCH_PARENT,
@@ -49,21 +56,14 @@ class AttendeesTicketType : BottomSheetDialogFragment() {
             ticketTypeRadioGroup.addView(radioButton)
         }
 
+        ticketTypeRadioGroup.setOnCheckedChangeListener{_, _ ->
+            val selectedId = ticketTypeRadioGroup.checkedRadioButtonId
+            val selectedRadioButton = view.findViewById<RadioButton>(selectedId)
+            val selectedValue = selectedRadioButton?.tag.toString()
 
-//        ticketTypeRadioGroup.setOnCheckedChangeListener{ group,checkID -.
-//
-//        }
-//
-//        val selectedId = ticketTypeRadioGroup.checkedRadioButtonId
-//        if (selectedId != -1) {
-//            val selectedRadioButton = view.findViewById<RadioButton>(selectedId)
-//            val selectedValue = selectedRadioButton?.tag.toString()
-//
-//            Toast.makeText(requireActivity(), "Yang dipilih: $selectedValue", Toast.LENGTH_SHORT).show()
-//        }
-//
-//
-//
+            Toast.makeText(requireActivity(), "Yang dipilih: $selectedValue", Toast.LENGTH_SHORT).show()
+
+        }
 
         return view
     }
--- app/src/main/java/com/example/eventify/attendeesAdapter/SeatAdapter.kt
+++ app/src/main/java/com/example/eventify/attendeesAdapter/SeatAdapter.kt
@@ -5,9 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ToggleButton
 import androidx.recyclerview.widget.RecyclerView
 import com.example.eventify.ModelData.SeatModelData
 import com.example.eventify.R
+import com.google.android.material.button.MaterialButton
 
 class SeatAdapter(private val seatList: MutableList<SeatModelData>) :
     RecyclerView.Adapter<SeatAdapter.ViewHolder>(){
@@ -19,7 +21,7 @@ class SeatAdapter(private val seatList: MutableList<SeatModelData>) :
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        val seatButton: Button =  itemView.findViewById(R.id.seat_button)
+        val seatButton: MaterialButton =  itemView.findViewById(R.id.seat_button)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -29,7 +31,6 @@ class SeatAdapter(private val seatList: MutableList<SeatModelData>) :
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val seat = seatList[position]
         holder.seatButton.text = seat.label
-
         when(seat.isSelected){
             true -> holder.seatButton.setBackgroundColor(Color.parseColor("#A62C2C"))
             false-> holder.seatButton.setBackgroundColor(Color.parseColor("#5F8B4C"))
