9383ffa9c2d1997bdbb81016be2912ba72b3020b
Wellen Breitner <wellenhan45@gmail.com>
2025-05-27 19:44:19 +0800

    adding detail

--- app/src/main/java/com/example/eventify/ModelData/BookedSeatModelData.kt
+++ app/src/main/java/com/example/eventify/ModelData/BookedSeatModelData.kt
@@ -1,5 +1,5 @@
 package com.example.eventify.ModelData
-
+// not used
 data class BookedSeatModelData(
     private val eventID: String = "",
     private val bookedSeatNumber: ArrayList<String>
--- app/src/main/java/com/example/eventify/ModelData/SeatModelData.kt
+++ app/src/main/java/com/example/eventify/ModelData/SeatModelData.kt
@@ -1,5 +1,5 @@
 package com.example.eventify.ModelData
-
+// only use for seat setup not for database
 data class SeatModelData(
     val label: String,
     val row: String,
